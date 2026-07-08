e4694422275861de53f4513d2524fe4fc81c9b43
Harshit <affiliate.couponfollow.net>
2026-07-08 13:20:36 +0530

    feat: add 1-year TTL auto-deletion index to ActivityLog model

--- server/src/models/ActivityLog.js
+++ server/src/models/ActivityLog.js
@@ -25,5 +25,7 @@ const activityLogSchema = new mongoose.Schema(
 
 activityLogSchema.index({ user: 1, createdAt: -1 });
 activityLogSchema.index({ action: 1 });
+// Auto-delete logs older than 1 year (365 days = 31536000 seconds)
+activityLogSchema.index({ createdAt: 1 }, { expireAfterSeconds: 31536000 });
 
 module.exports = mongoose.model('ActivityLog', activityLogSchema);
